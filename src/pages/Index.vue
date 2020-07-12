e64f91f49a1a6fc1117005d34a112f124bb425dd
alexanderkif <alexander_kif@mail.ru>
2020-07-12 22:28:25 +0400

    added text-center to index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
 <q-page class="flex flex-center">
-    <div class="text-h5">
+    <div class="text-h5 text-center">
       PageIndex. The test task is to render the page "Цвет свечения". Click "Цвет свечения" tab.
     </div>
   </q-page>
